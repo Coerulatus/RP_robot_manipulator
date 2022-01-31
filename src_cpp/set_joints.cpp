008d98f700f059e53fff31ddae71ba46c755b4d8
Coerulatus <marcomontagna21@gmail.com>
2022-01-31 12:12:17 +0100

    fixed set joints, it now works with shoulder's fixed links

--- src_cpp/set_joints.cpp
+++ src_cpp/set_joints.cpp
@@ -12,8 +12,7 @@ int main(int argc, char **argv) {
 	ros::init(argc, argv, "set_joints_node");
 	
 	ros::NodeHandle n;
-
-	int n_joints = 0;
+	
 	// get number of joints directly from DH_params
 	ifstream input;
   input.open("DH_params.txt");
@@ -25,47 +24,53 @@ int main(int argc, char **argv) {
   	}
   }
   
+  int n_joints = 0;
+	string s1, s2, s3, s4;
+	vector<int> moving_joint_idxs;
   char l[256];
   //ignore first line
   input.getline(l,256);
-  input.getline(l,256);
+  input>>s1>>s2>>s3>>s4;
   while(!input.eof()){
+  	moving_joint_idxs.push_back(n_joints+1);
+  	if(!stof(s2)==0)
+  		++n_joints;
+  	input>>s1>>s2>>s3>>s4;
   	++n_joints;
-  	input.getline(l,256);
   }
+  int n_moving_joints = moving_joint_idxs.size();
   
   vector<ros::Publisher> joints_pubs;
   vector<string> j_values;
   string topic;
-  for(int i=0;i<n_joints;++i){
-  	topic = "l"+to_string(i+1)+"_controller/command";
-  	joints_pubs.push_back(n.advertise<std_msgs::Float64>(topic, 100));
-  	j_values.push_back("0");
+  for(int i=0;i<n_moving_joints;++i){
+		topic = "l"+to_string(moving_joint_idxs[i])+"_controller/command";
+		joints_pubs.push_back(n.advertise<std_msgs::Float64>(topic, 100));
+		j_values.push_back("0");
   }
-   
+  /*for(int i=0;i<fixed_joint.size();i++)
+  	cout<<i<<" "<<fixed_joint[i]<<endl;*/
+  cout << "Waiting for the values for the joints. \nUse s to keep the joint still.\nType q to exit." << endl; 
+  int j_idx=0;
+  string j_v;
+	std_msgs::Float64 msg;
   while(ros::ok()){
-		cout << "Waiting for the values for the joints. \n Use s to keep the joint still.\n Type q to exit." << endl;
-		string j_v;
-		std_msgs::Float64 msg;
-		int j_idx=0;
-		while(cin >> j_v){
-			if(j_v=="q")
-				break;
-			else if(j_v=="s"){
-				cout << "Keeping joint "<<(j_idx%n_joints)+1<<" still"<<endl;
+		cin >> j_v;
+		if(j_v=="q")
+			break;
+		else if(j_v=="s"){
+			cout << "Keeping joint "<<(moving_joint_idxs[j_idx%n_moving_joints])<<" still"<<endl;
+			++j_idx;
+		}else{
+			try{
+				msg.data = stof(j_v);
+				joints_pubs[j_idx%n_moving_joints].publish(msg);
+				cout << "Setting position of joint " << (moving_joint_idxs[j_idx%n_moving_joints]) << " to: "<<j_v<<endl;
 				++j_idx;
-			}else{
-				try{
-					msg.data = stof(j_v);
-					joints_pubs[j_idx%n_joints].publish(msg);
-					cout << "Setting position of joint " << (j_idx%n_joints)+1 << " to: "<<j_v<<endl;
-					++j_idx;
-				}catch( ... ){
-					cout << "Invalid argument" <<endl;
-				}
+			}catch( ... ){
+				cout << "Invalid argument" <<endl;
 			}
 		}
-		break;
   }
 	return 0;
 }
